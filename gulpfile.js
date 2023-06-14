bd4668d4917ed02eadd288c2d753fa9174a0d6b1
Agustin <agusvigliocco@gmail.com>
2023-06-14 15:16:44 +0200

    creacion del nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber')
+
 
 function css(done) {
 
     src("src/scss/**/*.scss") // identificar el archivo de SASS
+        .pipe(plumber())
         .pipe(sass()) // lo compila
         .pipe(dest("build/css"));// lo almacena en el HDD
 
