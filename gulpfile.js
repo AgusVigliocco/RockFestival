2209d1fe958064ed6b1710e9f8b49e7cd410293a
Agustin <agusvigliocco@gmail.com>
2023-06-15 15:52:57 +0200

    creacion del header

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,69 @@
-const { src, dest, watch } = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
-const plumber = require('gulp-plumber')
+const { src, dest, watch, parallel } = require("gulp");
 
+//! CSS
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
+//! IMAGENES
+const webp = require("gulp-webp");
+const imagemin = require('gulp-imagemin')
+const cache = require('gulp-cache')
+const avif = require('gulp-avif')
+
+//! FUNCIONES
 function css(done) {
-
     src("src/scss/**/*.scss") // identificar el archivo de SASS
         .pipe(plumber())
         .pipe(sass()) // lo compila
-        .pipe(dest("build/css"));// lo almacena en el HDD
-
+        .pipe(dest("build/css")); // lo almacena en el HDD
 
     done();
 }
 
 function dev(done) {
+    watch("src/scss/**/*.scss", css);
 
-    watch("src/scss/**/*.scss", css)
+    done();
+}
+
+function versionWebp(done) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+
+function imagenes(done) {
+    const options = {
+        optimizationLevel: 3,
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(options)))
+        .pipe(dest('build/img'))
 
     done()
 }
 
-exports.css = css
-exports.dev = dev
+function versionAvif(done) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+
+
+exports.css = css;
+exports.imagenes = imagenes
+exports.versionAvif = versionAvif;
+exports.imagenes = imagenes
+exports.dev = parallel(versionAvif, imagenes, versionWebp, dev);
